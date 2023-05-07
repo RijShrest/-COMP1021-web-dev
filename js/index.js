bd0bcaed56fc8ad8d4afd4dee5b3a9fa52d1d179
Rijul Shrestha <sc22r2s@leeds.ac.uk>
2023-05-07 16:12:09 +0100

    Added geo-location feature

--- js/index.js
+++ js/index.js
@@ -2,13 +2,14 @@
 if (typeof (Storage) !== "undefined") {
     sessionStorage.setItem("currentPage", "home");
 } else {
-    alert("No local storage...")
+    alert("Local storage not supported...");
 }
 
 // Action during initial load
 $(document).ready(function () {
     loadPageLinks();
     homeClick();
+    getLocation();
 });
 
 // Nav bar button actions
@@ -82,7 +83,7 @@ function loadMyProjectContent() {
     $("#content-main").empty().load("../pages/projects.html");
 
     hidePageLinks();
-    $("#myProjectLinks").attr("hidden", false); 
+    $("#myProjectLinks").attr("hidden", false);
 }
 
 // Load contact.html content
@@ -95,10 +96,10 @@ function loadContactContent() {
 
 // Loads the links in the sidebar
 function loadPageLinks() {
-    $("#page-links").load("../pages/page-links.html", function() {
+    $("#page-links").load("../pages/page-links.html", function () {
         hidePageLinks();
         $("#homeLinks").attr("hidden", false);
-      });
+    });
 }
 
 
@@ -107,7 +108,7 @@ function hidePageLinks() {
     const container = document.querySelector("#page-links");
     const matches = container.querySelectorAll("div");
     matches.forEach((linkDiv) => {
-        linkDiv.hidden=true;
+        linkDiv.hidden = true;
     });
 }
 
@@ -134,4 +135,71 @@ function sendMessage() {
 
 function testClick() {
     clearActive();
+}
+
+// Geo-location
+
+var x = document.getElementById("geo-location-container");
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation not supported...");
+    }
+}
+
+function showPosition(position) {
+    var dist = distance(53.8, -1.55, position.coords.latitude, position.coords.longitude, "K");
+    x.innerHTML = "We are " + (Math.round((dist) * 100) / 100).toFixed(2) + "km apart";
+}
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+//:::                                                                         :::
+//:::  This routine calculates the distance between two points (given the     :::
+//:::  latitude/longitude of those points). It is being used to calculate     :::
+//:::  the distance between two locations using GeoDataSource (TM) prodducts  :::
+//:::                                                                         :::
+//:::  Definitions:                                                           :::
+//:::    South latitudes are negative, east longitudes are positive           :::
+//:::                                                                         :::
+//:::  Passed to function:                                                    :::
+//:::    lat1, lon1 = Latitude and Longitude of point 1 (in decimal degrees)  :::
+//:::    lat2, lon2 = Latitude and Longitude of point 2 (in decimal degrees)  :::
+//:::    unit = the unit you desire for results                               :::
+//:::           where: 'M' is statute miles (default)                         :::
+//:::                  'K' is kilometers                                      :::
+//:::                  'N' is nautical miles                                  :::
+//:::                                                                         :::
+//:::  Worldwide cities and other features databases with latitude longitude  :::
+//:::  are available at https://www.geodatasource.com                         :::
+//:::                                                                         :::
+//:::  For enquiries, please contact sales@geodatasource.com                  :::
+//:::                                                                         :::
+//:::  Official Web site: https://www.geodatasource.com                       :::
+//:::                                                                         :::
+//:::               GeoDataSource.com (C) All Rights Reserved 2022            :::
+//:::                                                                         :::
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function distance(lat1, lon1, lat2, lon2, unit) {
+    if ((lat1 == lat2) && (lon1 == lon2)) {
+        return 0;
+    }
+    else {
+        var radlat1 = Math.PI * lat1 / 180;
+        var radlat2 = Math.PI * lat2 / 180;
+        var theta = lon1 - lon2;
+        var radtheta = Math.PI * theta / 180;
+        var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+        if (dist > 1) {
+            dist = 1;
+        }
+        dist = Math.acos(dist);
+        dist = dist * 180 / Math.PI;
+        dist = dist * 60 * 1.1515;
+        if (unit == "K") { dist = dist * 1.609344 }
+        if (unit == "N") { dist = dist * 0.8684 }
+        return dist;
+    }
 }
